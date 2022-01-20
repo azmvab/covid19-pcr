face4163a4c88f3a54858a6863e1b3beb16408ed
Mukhammad Karimov <hey@karimoff.me>
2022-01-20 21:43:00 +0500

    fix3

--- travel/wsgi.py
+++ travel/wsgi.py
@@ -5,7 +5,9 @@ WSGI config for travel project.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
