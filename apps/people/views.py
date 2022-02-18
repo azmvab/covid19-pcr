9825c9cbd72e694a0d7fb6eb3313a69777f3a1da
Mukhammad Karimov <hey@karimoff.me>
2022-02-19 00:29:56 +0500

    fix images

--- apps/people/views.py
+++ apps/people/views.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.http import FileResponse
 from django.views import generic
 from PIL import Image, ImageDraw, ImageFont
@@ -19,9 +22,9 @@ class PeopleImageView(generic.DetailView):
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
 
-        img = Image.open("media/layer.jpg", mode='r')
+        img = Image.open(os.path.join(settings.MEDIA_ROOT, "layer.jpg"), mode='r')
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("media/font.ttf", size=46)
+        font = ImageFont.truetype(os.path.join(settings.MEDIA_ROOT, "font.ttf"), size=46)
         draw.text((550, 400), self.object.name, (0,0,0), font=font)
         draw.text((550, 505), self.object.identification_id, (0,0,0), font=font)
         draw.text((550, 610), self.object.birthday.isoformat(), (0,0,0), font=font)
@@ -31,6 +34,6 @@ class PeopleImageView(generic.DetailView):
         draw.text((1670, 610), self.object.result_time.isoformat(), (0,0,0), font=font)
         draw.text((1670, 715), self.object.report_no, (0,0,0), font=font)
         draw.text((1670, 820), self.object.hesn_no, (0,0,0), font=font)
-        img.save(f"media/images/layer-{self.object.guid}.jpg")
+        img.save(os.path.join(settings.MEDIA_ROOT, f"images/layer-{self.object.guid}.jpg"))
         
-        return FileResponse(open(f"media/images/layer-{self.object.guid}.jpg", 'rb'), content_type='image/jpeg')
+        return FileResponse(open(os.path.join(settings.MEDIA_ROOT, f"images/layer-{self.object.guid}.jpg"), 'rb'), content_type='image/jpeg')
