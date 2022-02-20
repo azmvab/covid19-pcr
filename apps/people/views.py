3aa56fe5e007cdeb5dd27349c2c9523a591029ba
Mukhammad Karimov <hey@karimoff.me>
2022-02-20 19:24:08 +0500

    qrcode logo

--- apps/people/views.py
+++ apps/people/views.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from django.http import FileResponse
 from django.views import generic
 from PIL import Image, ImageDraw, ImageFont
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.colormasks import VerticalGradiantColorMask
+from qrcode.image.styles.moduledrawers import CircleModuleDrawer
 
 from .models import Person
 
@@ -36,8 +39,14 @@ class PeopleImageView(generic.DetailView):
         draw.text((1670, 715), self.object.report_no, (0,0,0), font=font)
         draw.text((1670, 820), self.object.hesn_no, (0,0,0), font=font)
         url = request.build_absolute_uri(self.object.get_absolute_url())
-        qr_img = qrcode.make(url, version=7, box_size=10, border=4)
-        img.paste(qr_img, (280, 2870))
+        qr = qrcode.QRCode(version=7, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=2)
+        qr.add_data(url)
+        logo = Image.open(os.path.join(settings.MEDIA_ROOT, "logo.png"), mode='r')
+        logo.convert("RGBA")
+        logo = logo.resize((120, 180), Image.ANTIALIAS)
+        qr_img = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer(), color_mask=VerticalGradiantColorMask(back_color=(255, 255, 255), top_color=(190, 58, 56), bottom_color=(19, 8, 14)))
+        qr_img.paste(logo, (210, 170))
+        img.paste(qr_img, (300, 2870))
         img.save(os.path.join(settings.MEDIA_ROOT, f"images/layer-{self.object.guid}.jpg"))
         
         return FileResponse(open(os.path.join(settings.MEDIA_ROOT, f"images/layer-{self.object.guid}.jpg"), 'rb'), content_type='image/jpeg')
